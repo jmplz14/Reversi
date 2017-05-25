13ef5022551a53aabd27f007a5fd7975c2bd2808
jmplz14 <jmplz14@correo.ugr.es>
2017-05-25 22:44:51 +0200

    Se añade toda la parte de comprobar que una posicion es valida para la posicion seleccionada por el jugador al que pertenece el turno

--- include/tablero.h
+++ include/tablero.h
@@ -8,18 +8,37 @@ class Tablero{
 		bool turno_J1;
 		int n_fichas, n_fichas_colocadas;
 		int transformarCharCol( char col) const;
+		bool comprobarDiagonales(int fil , int col , int ficha) const;
+		bool comprobarRectas(int fil , int col , int ficha) const;
+		// a esta funcion se le pasa la fil y la columna aparte de lo valores que vamos a ir sumando
+		// estas para ir recorriendo el vector y ver si es una linea valida
+		bool recorrerMatrizCompro (int fil , int i_fils, int col , int i_cols, int ficha ) const;
+		// devuelve true si las coordenas estan dentro del tablero falso en caso contrario
+		bool comprobarCoordenadas(int fil , int col) const;
+		// comprobar ficha opuesta devuelve true si la ficha no es ella misma 
+		// ejemplo: si ficha es 1 devolvera falso si se envia un 2 o un 0
+		bool comprobarFichaOpuesta(int ficha , int posicion) const;
+		// Inf = inferior Sup = superor Der = derecha Izq=izquierda Dia=diagonal Rec=recta
+		bool comprobarDiaSupIzq(int fil , int col, int ficha) const;
+		bool comprobarDiaSupDer(int fil , int col, int ficha) const;
+		bool comprobarDiaInfIzq(int fil , int col, int ficha) const;
+		bool comprobarDiaInfDer(int fil , int col, int ficha) const;
+		bool comprobarRecIzq(int fil , int col, int ficha) const;
+		bool comprobarRecDer(int fil , int col , int ficha) const;
+		bool comprobarRecSup(int fil , int col , int ficha) const;
+		bool comprobarRecInf(int fil , int col , int ficha) const;
 	public:
 		Tablero();
 		Tablero(int fils);
 		Tablero(int fils, int cols);
 		int getFils() const;
 		int getCols() const;
-		int getPosicion(char cols, int fils) const;
+		int getPosicion(char col, int fil) const;
 		int turnoActual() const;
 		bool estadoTablero() const;
 		int obtenerGanador() const;
 		int obtenerPuntuacion(int jugador) const;
-		bool consultarPosicion(char cols, int fils) const;
+		bool consultarPosicion(char col, int fil) const;
 		bool posibilidadMovimiento(int jugador) const;
 		bool colocarFicha(char cols, int fils);
 		void vaciarTablero();
--- src/tablero.cpp
+++ src/tablero.cpp
@@ -53,9 +53,9 @@ Tablero::Tablero(int fils, int cols){
 	vaciarTablero();
 }
 
-int Tablero::getPosicion(char cols, int fils) const {
-	int entero_cols = transformarCharCol( cols );
-	return matriz.getPosition( fils, cols);
+int Tablero::getPosicion(char col, int fil) const {
+	int entero_cols = transformarCharCol( col );
+	return matriz.getPosition( fil, col);
 }
 
 int Tablero::turnoActual() const{
@@ -103,7 +103,125 @@ int Tablero::obtenerGanador() const{
 	return ganador;
 
 }
+bool Tablero::comprobarCoordenadas (int fil, int col ) const { 
+	return (fil >= 0 && fil < getFils() ) && ( col >= 0 && col < getCols() );
+}
+bool Tablero::comprobarFichaOpuesta(int ficha, int posicion) const{
+	return posicion != 0 && posicion != ficha;
+}
 
+bool Tablero::recorrerMatrizCompro (int fil , int i_fils, int col , int i_cols, int ficha ) const{
+	bool estado = false , dentro, opuesta;
+	int posicion;
+	
+	//se comprueba que la primera casilla no es ni una ficha igual ni esta vacia.
+	fil += i_fils; 
+	col += i_cols;
+	dentro = comprobarCoordenadas( fil, col );
+	
+	if (dentro){
+		do{
+			
+			posicion = getPosicion( fil, col );
+			opuesta = comprobarFichaOpuesta(ficha, posicion );
+			fil += i_fils; 
+			col += i_cols;
+			dentro = comprobarCoordenadas( fil, col );
+			
+		}while (dentro && opuesta);
+			estado = posicion == ficha ? true : false;
+	}
+	return estado;
+}
+bool Tablero::comprobarDiaSupIzq(int fil , int col, int ficha) const {
+	return recorrerMatrizCompro( fil, -1 , col, -1, ficha ); 
+}
+
+bool Tablero::comprobarDiaSupDer(int fil , int col, int ficha) const{
+	return recorrerMatrizCompro( fil, -1 , col, 1, ficha ); 
+}
+
+bool Tablero::comprobarDiaInfIzq(int fil , int col, int ficha) const{
+	return recorrerMatrizCompro( fil, 1 , col, -1, ficha ); 
+}
+
+bool Tablero::comprobarDiaInfDer(int fil , int col, int ficha) const{
+	return recorrerMatrizCompro( fil, 1 , col, 1, ficha ); 
+}
+
+bool Tablero::comprobarRecIzq(int fil , int col, int ficha) const{
+	return recorrerMatrizCompro( fil, 0 , col, -1, ficha ); 
+}
+bool Tablero::comprobarRecDer(int fil , int col , int ficha) const{
+	return recorrerMatrizCompro( fil, 0 , col, 1, ficha ); 
+}
+
+bool Tablero::comprobarRecSup(int fil , int col , int ficha) const{
+	return recorrerMatrizCompro( fil, -1 , col, 0, ficha ); 
+}
+
+bool Tablero::comprobarRecInf(int fil , int col , int ficha) const{
+	return recorrerMatrizCompro( fil, 1 , col, 0, ficha ); 
+}
+
+
+
+bool Tablero::comprobarDiagonales(int fil, int col, int ficha) const{
+	bool estado = false;
+	
+	//diagonal superior hacia la derecha la fila -1 a la columna +1
+	estado = comprobarDiaSupDer( fil, col, ficha ); 
+	
+	//diagonal superior hacia la izquierda la fila -1 a la columna -1
+	if( !estado )  
+		estado = comprobarDiaSupIzq( fil, col, ficha ); 			
+	
+	//diagonal inferior hacia la derecha la fila +1 a la columna +1
+	if( !estado )  
+		estado = comprobarDiaInfDer( fil, col, ficha ); 
+	
+	//diagonal inferior hacia la izquierda la fila +1 a la columna -1
+	if( !estado )  
+		estado = comprobarDiaInfIzq( fil, col, ficha ); 
+	
+	return estado;
+}
+
+bool Tablero::comprobarRectas(int fil, int col, int ficha) const{
+	bool estado = false;
+	
+	// superior a la fila -1 la columna siempre igual
+	estado = comprobarRecSup( fil, col, ficha ); 
+	
+	// inferior a la fila 1 la columna siempre igual
+	if( !estado )  
+		estado = comprobarRecInf( fil, col, ficha ); 			
+	
+	//derecha la fila siempre igual a la columna 1
+	if( !estado )  
+		estado = comprobarRecDer( fil, col, ficha ); 
+	
+	//izquierda la fila siempre igual a la columna -1
+	if( !estado )  
+		estado = comprobarRecIzq( fil, col, ficha ); 
+	
+	return estado;
+}
+
+
+bool Tablero::consultarPosicion(char col, int fil) const{
+	int entero_col = transformarCharCol(col), ficha;
+	bool estado = false;
+	if ( getPosicion( col , fil ) == 0){
+		ficha = turnoActual();
+		estado = comprobarDiagonales( fil, entero_col, ficha );
+		if ( !estado )
+			estado = comprobarRectas( fil, entero_col, ficha );
+				
+	}
+	
+	return estado;
+}
 
 
 
